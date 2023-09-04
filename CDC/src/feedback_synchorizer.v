ad687d482850ddde337df47324ab69baefa88df2
Alfre-D-Wen <alfred-wen@hotmail.com>
2023-09-04 16:30:07 +0800

    🐞 fix: 修改模块名称，移除reset信号，输出改为pulse

--- CDC/src/feedback_synchorizer.v
+++ CDC/src/feedback_synchorizer.v
@@ -18,79 +18,82 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns/1ns
-module feedback_synchorizer(
-        input in_clk_send,
-        input in_data,
-        input in_reset_send,
-        input in_clk_receive,
-        input in_reset_receive,
-        output out_data
-    );
+module feedback_synchronizer(
+           input in_clk_send,
+           input in_data,
+           input in_reset_send,
+           input in_clk_receive,
+           input in_reset_receive,
+           output out_data
+       );
 
-    reg send_data_r;
-    reg send_data_r_d1;
-    reg send_data_ack_r;
-    reg send_data_ack_d1_r;
-    reg receive_data_r;
-    reg receive_data_d1_r;
+reg send_data_r;
+reg send_data_r_d1;
+reg send_data_ack_r;
+reg send_data_ack_d1_r;
+reg receive_data_r;
+reg receive_data_d1_r;
+reg receive_data_d2_r;
 
-    /**************************SEND_DOMAIN***********************************/
-    //!send_data_r & send_data_r_d1
-    always @(posedge in_clk_send or posedge in_reset_send)
+/**************************SEND_DOMAIN***********************************/
+//!send_data_r & send_data_r_d1
+always @(posedge in_clk_send)
+begin
+    if (~in_reset_send)
     begin
-        if (in_reset_send)
-        begin
-            send_data_r <= 1'b0;
-            send_data_r_d1 <= send_data_r;
-        end
-        else if (in_data)
-        begin
-            send_data_r <= 1'b1;
-            send_data_r_d1 <= send_data_r;
-        end
-        else if (send_data_ack_d1_r)
-        begin
-            send_data_r <= 1'b0;
-            send_data_r_d1 <= send_data_r;
-        end
-        else
-        begin
-            send_data_r <= send_data_r;
-            send_data_r_d1 <= send_data_r;
-        end
+        send_data_r <= 1'b0;
+        send_data_r_d1 <= send_data_r;
     end
-
-    //!send_data_ack_r
-    always @(posedge in_clk_send or posedge in_reset_send)
+    else if (in_data)
     begin
-        if (in_reset_send)
-        begin
-            send_data_ack_r <= 1'b0;
-            send_data_ack_d1_r <= 1'b0;
-        end
-        else
-        begin
-            send_data_ack_r <= receive_data_d1_r;
-            send_data_ack_d1_r <= send_data_ack_r;
-        end
+        send_data_r <= 1'b1;
+        send_data_r_d1 <= send_data_r;
     end
-    /**************************RECEIVE_DOMAIN***********************************/
-    //!receive_data_r,receive_data
-    always @(posedge in_clk_receive or posedge in_reset_receive)
+    else if (send_data_ack_d1_r)
     begin
-        if (in_reset_receive)
-        begin
-            receive_data_r <= 1'b0;
-            receive_data_d1_r <= 1'b0;
-        end
-        else
-        begin
-            receive_data_r <= send_data_r_d1;
-            receive_data_d1_r <= receive_data_r;
-        end
+        send_data_r <= 1'b0;
+        send_data_r_d1 <= send_data_r;
     end
+    else
+    begin
+        send_data_r <= send_data_r;
+        send_data_r_d1 <= send_data_r;
+    end
+end
 
-    //!out_data
-    assign out_data = receive_data_d1_r;
+//!send_data_ack_r
+always @(posedge in_clk_send)
+begin
+    if (~in_reset_send)
+    begin
+        send_data_ack_r <= 1'b0;
+        send_data_ack_d1_r <= 1'b0;
+    end
+    else
+    begin
+        send_data_ack_r <= receive_data_d1_r;
+        send_data_ack_d1_r <= send_data_ack_r;
+    end
+end
+/**************************RECEIVE_DOMAIN***********************************/
+//!receive_data_r,receive_data
+always @(posedge in_clk_receive)
+begin
+    if (~in_reset_receive)
+    begin
+        receive_data_r <= 1'b0;
+        receive_data_d1_r <= 1'b0;
+        receive_data_d2_r <= 1'b0;
+    end
+    else
+    begin
+        receive_data_r <= send_data_r_d1;
+        receive_data_d1_r <= receive_data_r;
+        receive_data_d2_r <= receive_data_d1_r;
+    end
+end
+
+//!out_data
+assign out_data = receive_data_d1_r && (~receive_data_d2_r);
 
 endmodule
